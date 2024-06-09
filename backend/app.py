6b8547094a8be613305b6713bdac7c0ae454f55b
Nwali Somto <somtobernardnwali619@gmail.com>
2024-06-09 17:47:38 +0100

    4- configure app.py for deployment

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -9,6 +10,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friends.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
 db = SQLAlchemy(app)
+
+
+frontend_folder = os.path.join(os.getcwd(), "..", "frontend")
+dist_folder = os.path.join(frontend_folder, "dist")
+
+@app.route("/", defaults={"filename": ""})
+@app.route("/<path:filename>")
+def index(filename):
+  if not filename:
+    filename = "index.html"
+  return send_from_directory(dist_folder, filename)
 
 import routes
 
